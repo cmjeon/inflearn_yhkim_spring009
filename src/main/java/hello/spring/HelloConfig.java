275094031f70bd27c09ee3e8db81477048dfe0bc
cmj <chmin82@gmail.com>
2024-02-06 23:07:52 +0900

    스프링 부트와 내장 톰캣 : 편리한 부트 클래스 만들기

--- src/main/java/hello/spring/HelloConfig.java
+++ src/main/java/hello/spring/HelloConfig.java
@@ -1,13 +1,13 @@
 package hello.spring;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class HelloConfig {
 
     @Bean
     public HelloController helloController() {
         return new HelloController();
     }
+
 }
